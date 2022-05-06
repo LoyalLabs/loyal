8e1d8bcb3c8b7eb54ff77baccf1fbe51a3670061
ubuntu <ubuntu@tndrmnt1.loyal>
2022-05-06 02:08:41 -0400

    Initial commit

---
+++ x/loyal/keeper/msg_server_test.go
@@ -0,0 +1,16 @@
+package keeper_test
+
+import (
+	"context"
+	"testing"
+
+	keepertest "github.com/LoyalLabs/loyal/testutil/keeper"
+	"github.com/LoyalLabs/loyal/x/loyal/keeper"
+	"github.com/LoyalLabs/loyal/x/loyal/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
+	k, ctx := keepertest.LoyalKeeper(t)
+	return keeper.NewMsgServerImpl(*k), sdk.WrapSDKContext(ctx)
+}
