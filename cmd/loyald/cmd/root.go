a6fad8daa6846352608749b4629a31b2a6c2d53d
monopauli <steamleopac@gmail.com>
2022-11-07 16:13:21 +0100

    braces correction

--- cmd/loyald/cmd/root.go
+++ cmd/loyald/cmd/root.go
@@ -232,11 +232,7 @@ func overwriteFlagDefaults(c *cobra.Command, defaults map[string]string) {
 			if err := f.Value.Set(val); err != nil {
 				panic(err)
 			}
-<<<<<<< HEAD
-	}
-=======
 		}
->>>>>>> gofumpt
 	}
 	for key, val := range defaults {
 		set(c.Flags(), key, val)
